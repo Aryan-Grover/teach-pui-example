5c7cb3aa84205076b859b400e2a7439f50549390
Aryan-Grover <112531059+Aryan-Grover@users.noreply.github.com>
2022-10-06 21:03:40 -0400

    HW5

---
+++ solution-hw5/Cartpage.js
@@ -0,0 +1,111 @@
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing = rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+let cart = new Set();
+let productList = [];
+let Grandtotal=0.0;
+
+let Roll1 = new Roll("Original", "Sugar Milk", 1, 2.49);
+let Roll2 = new Roll("Walnut", "Vanilla Milk", 12, 3.49);
+let Roll3 = new Roll("Raisin", "Sugar Milk", 3,  2.99);
+let Roll4 = new Roll("Apple", "Original", 3, 3.49);
+
+productList.push(Roll1, Roll2, Roll3, Roll4);
+
+
+class Glaze {
+    glazetype;
+    glazeprice;
+
+    constructor(glazetype, glazeprice) {
+        this.glazetype = glazetype;
+        this.glazeprice = glazeprice;
+    }
+}
+
+//glazing object
+const keeporiginal = new Glaze("Keep Original", 0);
+const sugarmilk = new Glaze("Sugar Milk", 0);
+const vanillamilk = new Glaze("Vanilla Milk", 0.50);
+const doublechocolate = new Glaze("Double Chocolate", 1.50);
+
+//pack constructor
+class Packs {
+    packsize;
+    packprice;
+
+    constructor(packsize, packprice) {
+        this.packsize = packsize;
+        this.packprice = packprice;
+    }
+}
+//pack object
+const onepack = new Packs(1, 1);
+const threepack = new Packs(3, 3);
+const sixpack = new Packs(6, 5);
+const twelvepack = new Packs(12, 10);
+
+let allglazes = [keeporiginal, sugarmilk, vanillamilk, doublechocolate];
+let allpacks = [onepack, threepack, sixpack, twelvepack];
+
+
+function calculatetotalprice(basePrice, rollGlazing, packPrice) {
+    return ((basePrice + rollGlazing) * packPrice)
+}
+
+for (let i = 0; i < productList.length; i++) {
+
+    let newRoll = productList[i];
+    cart.add(newRoll);
+
+    let cartitem = document.getElementsByTagName("template")[0];
+    let cartitemClone = cartitem.content.cloneNode(true);
+
+    cartitemClone.querySelector(".rollname").innerText = productList[i].type + " Cinnamon Roll";
+    cartitemClone.querySelector(".glazename").innerText = "Glazing: " + productList[i].glazing;
+    cartitemClone.querySelector(".pack-size").innerText = "Pack Size: " + productList[i].size;
+
+    cartitemClone.querySelector(".cart-image").src = rolls[productList[i].type].imageFile;
+    
+    let glazingprice;
+    let packPrice = allpacks.find(item => item.packsize == productList[i].size).packprice
+
+    if(productList[i].glazing == "Original"){
+        glazingprice = keeporiginal.glazeprice;
+    }
+    else{
+        glazingprice = allglazes.find(item => item.glazetype == productList[i].glazing).glazeprice;
+    }
+
+    let x = (calculatetotalprice(productList[i].basePrice,glazingprice,packPrice)).toFixed(2);
+    finalTotal= "$ "+ x;
+
+    cartitemClone.querySelector(".cart-text2").innerText = finalTotal;
+
+    Grandtotal = Grandtotal + parseFloat(x);
+
+    document.querySelector(".cart-text-final").innerText = "$ " + Grandtotal;
+    let c = cartitemClone.querySelector(".cart1-product")
+
+
+    cartitemClone.querySelector("button").onclick = (() => {
+        c.remove();
+        let rPrice = c.querySelector(".cart-text2").innerText.slice(2);
+        let rTotal = document.querySelector(".cart-text-final").innerText.slice(2);
+        document.querySelector(".cart-text-final").innerText = "$ "+ (rTotal-rPrice).toFixed(2);
+        cart.delete(newRoll);
+        console.log(cart);
+})
+
+
+    document.querySelector(".Product-list").appendChild(cartitemClone);
+
+}
+
+
---
+++ solution-hw5/function.js
@@ -0,0 +1,118 @@
+//glazing constructor
+class Glaze {
+  glazetype;
+  glazeprice;
+
+  constructor(glazetype, glazeprice) {
+    this.glazetype = glazetype;
+    this.glazeprice = glazeprice;
+  }
+}
+
+//glazing object
+const keeporiginal = new Glaze("Keep Original", 0);
+const sugarmilk = new Glaze("Sugar Milk", 0);
+const vanillamilk = new Glaze("Vanilla milk", 0.50);
+const doublechocolate = new Glaze("Double chocolate", 1.50);
+
+//pack constructor
+class Packs {
+  packsize;
+  packprice;
+
+  constructor(packsize, packprice) {
+    this.packsize = packsize;
+    this.packprice = packprice;
+  }
+}
+//pack object
+const onepack = new Packs(1, 1);
+const threepack = new Packs(3, 3);
+const sixpack = new Packs(6, 5);
+const twelvepack = new Packs(12, 10);
+
+//array for glaze and pack objects
+let allglazes = [keeporiginal, sugarmilk, vanillamilk, doublechocolate];
+let allpacks = [onepack, threepack, sixpack, twelvepack];
+
+let glazeoptions = document.getElementById("glazing");
+
+//loop for glaze dropdown
+for (i = 0; i < allglazes.length; i++) {
+  let glazeoptionitem = document.createElement("option");
+  glazeoptionitem.innerText = allglazes[i].glazetype;
+  glazeoptions.appendChild(glazeoptionitem);
+}
+
+//loop for packsize dropdown
+let packoptions = document.getElementById("packsize");
+for (i = 0; i < allpacks.length; i++) {
+  let packoptionitem = document.createElement("option");
+  packoptionitem.textContent = allpacks[i].packsize;
+  packoptions.appendChild(packoptionitem);
+}
+
+let glazechoice = 0;
+let packchoice = 1;
+
+//function to change the price
+function calculatePrice(element) {
+  let baseprice = rolls[rollType].basePrice;
+  let finalprice = 0;
+
+  const pricechange = element.value;
+
+  for (i = 0; i < allglazes.length; i++) {
+    if (allglazes[i].glazetype == element.value) {
+      glazechoice = allglazes[i].glazeprice;
+    }
+  }
+
+
+
+  for (i = 0; i < allpacks.length; i++) {
+    if (allpacks[i].packsize == element.value) {
+      packchoice = allpacks[i].packprice;
+    }
+  }
+
+  finalprice = parseFloat((baseprice + glazechoice) * packchoice).toFixed(2);
+
+  document.getElementById("finalprice").innerHTML = "$ " + finalprice;
+
+}
+
+//Homework-4//
+
+let cart = [];
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get("roll");
+
+
+let productHeading = document.querySelector(".cin-heading");
+let productPrice = document.querySelector(".card-total");
+let productImage = document.querySelector(".product-gallery-image");
+
+productHeading.innerText = rollType + " Cinnamon Roll";
+productPrice.innerText = rolls[rollType].basePrice;
+productImage.src = rolls[rollType].imageFile;
+
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+    this.type = rollType;
+    this.glazing = rollGlazing;
+    this.size = packSize;
+    this.basePrice = basePrice;
+  }
+}
+
+//Function to add item to the cart //
+function AddtoCart() {
+  let glazing = glazeoptions.value;
+  let packSize = packoptions.value;
+  let basePrice = rolls[rollType].basePrice;
+  let r = new Roll(rollType, glazing, packSize, basePrice);
+  cart.push(r);
+  console.log(cart);
+}
