a31faeae317e12c38b588f0e640a5df09074b23c
Aryan-Grover <112531059+Aryan-Grover@users.noreply.github.com>
2022-12-04 13:57:48 -0500

    Project

---
+++ final/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <aside>
+    <div class="nav-container">
+      <img
+        src="assets/group1.png"
+        alt= "Random"
+      />
+      </div>
+</main>
+
+<style>
+</style>
